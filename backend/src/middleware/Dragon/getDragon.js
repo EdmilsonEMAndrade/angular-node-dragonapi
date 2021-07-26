35ac8e44b09381b7247b43ee01394559529bff91
Edmilson Andrade <edmilson.andrad@gmail.com>
2021-07-26 11:39:11 -0300

    Ddockerfile

--- backend/src/middleware/Dragon/getDragon.js
+++ backend/src/middleware/Dragon/getDragon.js
@@ -3,14 +3,14 @@ const DragonConstants = require('../../constants/DragonConstants');
 const HttpConstants = require('../../constants/http');
 const Cache = require('../../utils/Cache');
 
-async function getDragon(req, res, next) {
-    
+async function getDragon(req, res, next) {     
+   
     try {
         dragon = await Dragon.findById(req.params.id);
     } catch (err) {
         return res.status(HttpConstants.BadRequest).json({ message: DragonConstants.InvalidID });
     }
-    
+       
     res.dragon = dragon;
     next();
 }
